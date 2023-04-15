e802fc290c1afde24cdb7c42561432bbbfd60e29
Juani <juan.battelli@gmail.com>
2023-04-14 22:32:38 -0300

    Seccion Customers terminada

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        //Lista hardcodeada de customers para testear
+        List<Customer> customers = new List<Customer>
+            {
+                new Customer { Name = "Pepe Argento", Id = 1},
+                new Customer { Name = "Robert Smith", Id = 2}
+            };
+
+        [Route("Customers")]
+        public IActionResult Index()
+        {
+            var viewModel = new CustomersViewModel { Customers = customers };
+
+            return View(viewModel);
+        }
+
+        public IActionResult Details(int id)
+        {
+            //SingleOrDefault devuelve un valor default (null en este caso) si no encuentra coincidencia
+            var customer = customers.SingleOrDefault(c => c.Id == id);
+
+            //En caso de ser null error 404.
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            return View(customer);
+        }
+
+    }
+}
