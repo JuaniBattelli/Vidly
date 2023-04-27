be4cdce1ea03ff7351c7b67999620613da73a930
Juani <juan.battelli@gmail.com>
2023-04-27 13:53:15 -0300

    Validation

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -54,12 +54,20 @@ namespace Vidly.Controllers
         [HttpPost]
         public IActionResult Save(Customer customer)
         {
+            if (!ModelState.IsValid)
+            {
+                var viewModel = new CustomerFormViewModel {
+                    Customer = customer,
+                    MembershipTypes = _context.MembershipTypes.ToList()
+                };
+                return View("CustomerForm", viewModel);
+            }
+
             if(customer.Id == 0)
                 _context.Customers.Add(customer);
             else
             {
                 var customerInDb = _context.Customers.Single(c => c.Id == customer.Id);
-
 
                 //Mapper.Map(customer, customerInDb); // AUTO MAPPER
                 customerInDb.Name = customer.Name;
--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -7,17 +7,19 @@ namespace Vidly.Models
     {
         public int Id { get; set; }
         
-        [Required]
+        [Required(ErrorMessage = "Please enter a customer's name.")]
         [StringLength(255)]
-        public string Name { get; set; }
+        public string? Name { get; set; }
         public bool isSubscribedToNewsletter { get; set; }
         public MembershipType MembershipType { get; set; }
-        
+
         [Display(Name = "Membership Type")]
-        public byte MembershipTypeId { get; set; }
+        [Required(ErrorMessage = "The Membership Type field is required.")]
+        [Range(1, 4, ErrorMessage = "Please select a valid Membership Type.")]
+        public byte? MembershipTypeId { get; set; }
         
-        [AllowNull]
         [Display(Name = "Date of Birth")]
+        [Min18YearsIfAMember]
         public DateTime? Birthday { get; set; }
 
     }
